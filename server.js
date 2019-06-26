70d66261a38658754d31c007bf2db5b17e86c9b2
Gabriel Simmer <gsimmer@acromedia.com>
2019-06-26 16:27:38 -0700

    Debugging server .

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 // Super
-
 const http = require('http');
 const fs = require('fs');
+
+console.log("Starting up server")
 
 fs.readFile('index.html', function (err, html) {
     if (err) {
         throw err;
     }
+    console.log("Read index file...")
 
     const server = http.createServer((request, response) => {
         response.writeHead(200, { "Content-Type": "text/html" });
